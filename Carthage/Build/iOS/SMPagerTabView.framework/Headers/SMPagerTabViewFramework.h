9e9d2dc8d4fbe4c3af2bb0532db7ba888af94df3
547 <sevenwang@SevenWangdeiMac.local>
2017-02-23 11:29:20 -0700

    打包成Framework

---
+++ Carthage/Build/iOS/SMPagerTabView.framework/Headers/SMPagerTabViewFramework.h
@@ -0,0 +1,19 @@
+//
+//  SMPagerTabViewFramework.h
+//  SMPagerTabViewFramework
+//
+//  Created by SevenWang on 2017/2/23.
+//  Copyright © 2017年 SevenWang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SMPagerTabViewFramework.
+FOUNDATION_EXPORT double SMPagerTabViewFrameworkVersionNumber;
+
+//! Project version string for SMPagerTabViewFramework.
+FOUNDATION_EXPORT const unsigned char SMPagerTabViewFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SMPagerTabViewFramework/PublicHeader.h>
+
+
